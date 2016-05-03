2011039900b6c789a302363e5a1867fab55d49f7
OmarKiyani <omar.kiyani@gmail.com>
2016-05-02 22:06:36 -0400

    more changes3

--- app.js
+++ app.js
@@ -44,8 +44,6 @@ app.use('/users', users);
 app.use('/restaurants', restaurants);
 app.use('/menus', menus);
 
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
